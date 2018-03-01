3e4f9ef664950f9249b8e6173008bb85f8ee6a4c
Fredi <fredi.sa@hotmail.de>
2018-03-01 19:36:10 +0100

    check if possible moved to class

--- src/hashcode/Main.java
+++ src/hashcode/Main.java
@@ -23,18 +23,5 @@ public class Main {
         //TODO Output
     }
 
-    public boolean checkIfPossible (int myX, int myY, int fromX, int toX, int fromY, int toY, int currentTime, int earliestStart, int latestFinish) {
 
-        if (abs(fromX - toX) + abs(fromY - toY) <= latestFinish - earliestStart) {
-            // Possible at all
-            if (abs(myX - fromX) + abs(myY - fromY) + abs(fromX - toX) + abs(fromY - toY) <= (latestFinish - currentTime)) {
-                //Possible from current position
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
 }
