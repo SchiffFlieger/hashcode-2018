4180ff2bc56f96bf3066151046eb1b1a74cdfd35
Fredi <fredi.sa@hotmail.de>
2018-03-01 19:27:41 +0100

    check if possible

--- src/hashcode/Main.java
+++ src/hashcode/Main.java
@@ -1,7 +1,25 @@
 package hashcode;
 
+import static java.lang.Math.abs;
+
 public class Main {
     public static void main (String[] args) {
         System.out.println("hello hashcode");
+
+    }
+
+    public boolean checkIfPossible (int myX, int myY, int fromX, int toX, int fromY, int toY, int currentTime, int earliestStart, int latestFinish) {
+
+        if (abs(fromX - toX) + abs(fromY - toY) <= latestFinish - earliestStart) {
+            // Possible at all
+            if (abs(myX - fromX) + abs(myY - fromY) + abs(fromX - toX) + abs(fromY - toY) <= (latestFinish - currentTime)) {
+                //Possible from current position
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
